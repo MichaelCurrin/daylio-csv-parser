e0a9d3fe7192dc668ab2b54ffd24203b9788fe4c
MichaelCurrin <michael.currin@gmail.com>
2018-05-19 15:51:30 +0200

    feature: Create setup_db.sql

---
+++ tools/setup_db.sql
@@ -0,0 +1,8 @@
+/* When importing a multiple times, it is useful to clear existing any
+   data first. */
+DROP TABLE IF EXISTS daylio;
+
+.mode csv
+
+/* Usage: .import FILE TABLE */
+.import var/data_out/cleaned.csv daylio
